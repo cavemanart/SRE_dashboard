3d34cad1258e45e0fe712c9f48a037ccd972b7b4
cavemanart <108423418+cavemanart@users.noreply.github.com>
2025-12-17 10:35:00 -0500

    Remove blank line in render_top_bar function
    Removed an unnecessary blank line in the render_top_bar function.

--- components/top_bar.py
+++ components/top_bar.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 def render_top_bar():
     col1, col2, col3, col4 = st.columns(4)
-
     col1.metric("Health Score", "92", "+2")
     col2.metric("Active Incidents", "1")
     col3.metric("Error Budget", "78%")
